0435899b15bb7cb22ddf3a3356baade8f0fc5044
Roman Shevtsov <roman.shevtsov@gmail.com>
2015-07-13 11:28:09 +0200

    Initial Commit

---
+++ circles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  circles
+//
+//  Created by Roman on 13/07/15.
+//  Copyright (c) 2015 rshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
